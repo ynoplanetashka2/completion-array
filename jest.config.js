c733d112ae77e1359196078c02dd7377de0b1f4d
ynoplanetashka2 <mrynoplanetashka@gmail.com>
2022-11-02 06:55:08 +0500

    style: fixed jest.config style, tab_width setted

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 // @ts-check
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': [
-      'ts-jest',
-      {
-        tsconfig: './tsconfig.jest.json'
-      }
-    ]
-  }
+	testEnvironment: 'node',
+	transform: {
+		'.+\\.ts$': [
+			'ts-jest',
+			{
+				tsconfig: './tsconfig.jest.json'
+			}
+		]
+	}
 };
